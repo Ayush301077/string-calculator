1fb6721df15fb81ea80963e7c52952d15b8d23b0
Ayush301077 <luckyvsn0904@gmail.com>
2025-07-04 22:59:30 +0530

    Update Add to ignore numbers greater than 1000 in the sum

--- src/main/java/com/incubyte/stringcalculator/StringCalculator.java
+++ src/main/java/com/incubyte/stringcalculator/StringCalculator.java
@@ -4,33 +4,33 @@ public class StringCalculator {
     private int callCount = 0;
 
     public int Add(String numbers) {
-        callCount++;
-
-        if (numbers.isEmpty()) {
-            return 0;
-        }
-        String delimiter = ",|\n";
-        String nums = numbers;
-        if (numbers.startsWith("//")) {
-            delimiter = Character.toString(numbers.charAt(2));
-            nums = numbers.substring(4);
-        }
-        String[] numArray = nums.split(delimiter);
-        int sum = 0;
-        StringBuilder negatives = new StringBuilder();
-        for (String num : numArray) {
-            int value = Integer.parseInt(num);
-            if (value < 0) {
-                if (negatives.length() > 0) negatives.append(",");
-                negatives.append(value);
-            }
+    callCount++;
+    if (numbers.isEmpty()) {
+        return 0;
+    }
+    String delimiter = ",|\n";
+    String nums = numbers;
+    if (numbers.startsWith("//")) {
+        delimiter = Character.toString(numbers.charAt(2));
+        nums = numbers.substring(4);
+    }
+    String[] numArray = nums.split(delimiter);
+    int sum = 0;
+    StringBuilder negatives = new StringBuilder();
+    for (String num : numArray) {
+        int value = Integer.parseInt(num);
+        if (value < 0) {
+            if (negatives.length() > 0) negatives.append(",");
+            negatives.append(value);
+        } else if (value <= 1000) {
             sum += value;
         }
-        if (negatives.length() > 0) {
-            throw new IllegalArgumentException("negatives not allowed: " + negatives);
-        }
-        return sum;
     }
+    if (negatives.length() > 0) {
+        throw new IllegalArgumentException("negatives not allowed: " + negatives);
+    }
+    return sum;
+}
 
     public int GetCalledCount() {
         return callCount;
