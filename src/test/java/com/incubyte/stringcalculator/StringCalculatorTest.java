e566ee49f0a6387aaa3111c9b1f1301081818c31
Ayush301077 <luckyvsn0904@gmail.com>
2025-07-04 17:25:29 +0530

    Add failing test: should return 0 for empty string

---
+++ src/test/java/com/incubyte/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+package com.incubyte.stringcalculator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringCalculatorTest {
+    @Test
+    void shouldReturnZeroForEmptyString() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.Add(""));
+    }
+}
