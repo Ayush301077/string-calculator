8a89a0c2fe7b8cd4c3fded220064f41cbc80e219
Ayush301077 <luckyvsn0904@gmail.com>
2025-07-04 23:08:03 +0530

    Update Add to support delimiters of any length for given format

--- src/main/java/com/incubyte/stringcalculator/StringCalculator.java
+++ src/main/java/com/incubyte/stringcalculator/StringCalculator.java
@@ -1,36 +1,42 @@
 package com.incubyte.stringcalculator;
+import java.util.regex.*;
 
 public class StringCalculator {
     private int callCount = 0;
 
     public int Add(String numbers) {
-    callCount++;
-    if (numbers.isEmpty()) {
-        return 0;
-    }
-    String delimiter = ",|\n";
-    String nums = numbers;
-    if (numbers.startsWith("//")) {
-        delimiter = Character.toString(numbers.charAt(2));
-        nums = numbers.substring(4);
-    }
-    String[] numArray = nums.split(delimiter);
-    int sum = 0;
-    StringBuilder negatives = new StringBuilder();
-    for (String num : numArray) {
-        int value = Integer.parseInt(num);
-        if (value < 0) {
-            if (negatives.length() > 0) negatives.append(",");
-            negatives.append(value);
-        } else if (value <= 1000) {
-            sum += value;
+        callCount++;
+        if (numbers.isEmpty()) {
+            return 0;
         }
+        String delimiter = ",|\n";
+        String nums = numbers;
+        if (numbers.startsWith("//[")) {
+            int endIndex = numbers.indexOf("]\n");
+            delimiter = Pattern.quote(numbers.substring(3, endIndex));
+            nums = numbers.substring(endIndex + 2);
+        } else if (numbers.startsWith("//")) {
+            delimiter = Character.toString(numbers.charAt(2));
+            nums = numbers.substring(4);
+        }
+        String[] numArray = nums.split(delimiter);
+        int sum = 0;
+        StringBuilder negatives = new StringBuilder();
+        for (String num : numArray) {
+            if (num.isEmpty()) continue;
+            int value = Integer.parseInt(num);
+            if (value < 0) {
+                if (negatives.length() > 0) negatives.append(",");
+                negatives.append(value);
+            } else if (value <= 1000) {
+                sum += value;
+            }
+        }
+        if (negatives.length() > 0) {
+            throw new IllegalArgumentException("negatives not allowed: " + negatives);
+        }
+        return sum;
     }
-    if (negatives.length() > 0) {
-        throw new IllegalArgumentException("negatives not allowed: " + negatives);
-    }
-    return sum;
-}
 
     public int GetCalledCount() {
         return callCount;
