9343ee90ff21939fd145bb618fdfd20bc5be9063
Ayush301077 <luckyvsn0904@gmail.com>
2025-07-04 19:31:02 +0530

    Implement Add to add two numbers

--- src/main/java/com/incubyte/stringcalculator/StringCalculator.java
+++ src/main/java/com/incubyte/stringcalculator/StringCalculator.java
@@ -2,9 +2,13 @@ package com.incubyte.stringcalculator;
 
 public class StringCalculator {
     public int Add(String numbers) {
-        if (numbers.isEmpty()) {
-            return 0;
-        }
-        return Integer.parseInt(numbers);
+    if (numbers.isEmpty()) {
+        return 0;
+    }
+    if (numbers.contains(",")) {
+        String[] nums = numbers.split(",");
+        return Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
+    }
+    return Integer.parseInt(numbers);
 }
 }
