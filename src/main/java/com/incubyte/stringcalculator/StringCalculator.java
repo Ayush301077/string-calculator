6f86e5ac49505b7dc8d49b689fb7986c929739af
Ayush301077 <luckyvsn0904@gmail.com>
2025-07-04 22:45:26 +0530

    Update Add to list all negative numbers in exception

--- src/main/java/com/incubyte/stringcalculator/StringCalculator.java
+++ src/main/java/com/incubyte/stringcalculator/StringCalculator.java
@@ -13,12 +13,17 @@ public class StringCalculator {
         }
         String[] numArray = nums.split(delimiter);
         int sum = 0;
+        StringBuilder negatives = new StringBuilder();
         for (String num : numArray) {
             int value = Integer.parseInt(num);
             if (value < 0) {
-                throw new IllegalArgumentException("negatives not allowed: " + value);
+                if (negatives.length() > 0) negatives.append(",");
+                negatives.append(value);
             }
             sum += value;
+        }
+        if (negatives.length() > 0) {
+            throw new IllegalArgumentException("negatives not allowed: " + negatives);
         }
         return sum;
     }
